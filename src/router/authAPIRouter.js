a1635e1406181b5c1e1872f592b23fb5ce0cc86f
何丽彬 <helibin@139.com>
2018-07-17 00:38:10 +0800

    引入jsdoc

--- src/router/authAPIRouter.js
+++ src/router/authAPIRouter.js
@@ -1,16 +1,50 @@
-/** 内建模块 */
+// /** 内建模块 */
 
-/** 第三方模块 */
+// /** 第三方模块 */
 
-/** 基础模块 */
-import { router } from './base';
+// /** 基础模块 */
+import { router  } from './base';
 
-/** 项目模块 */
-import { authAPICtrl } from '../controller';
-import { ipMid, paramMid } from '../middleware';
-import { authLgc } from '../logic';
+// /** 项目模块 */
+import { authAPICtrl  } from '../controller';
+import {
+  ipMid,
+  paramMid,
+} from '../middleware';
+import { authLgc  } from '../logic';
 
 
+/**
+ * @api {get} /auth/sign-in 用户登录
+ * @apiName signIn
+ * @apiGroup Auth
+ *
+ * @apiParam {String} identifier 用户名.
+ * @apiParam {String} password密码(md5值).
+ *
+ * @apiSuccess {String} err 错误代码.
+ * @apiSuccess {String} msg  错误消息.
+ * @apiSuccess {Object} [data]  额外数据.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *        "err": 0,
+ *        "msg": "",
+ *        "data": userInfo
+ *        "requestId": "reqesutId"
+ *     }
+ *
+ * @apiError UserNotFound The id of the User was not found.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *        "err": 1060,
+ *        "msg": "用户名或密码错误",
+ *        "requestId": "reqesutId"
+ *     }
+ */
 router.post('/auth/sign-in',
   paramMid.check(authLgc.signIn),
   authAPICtrl.run('signIn'));
---
+++ src/static/apidoc/api_project.js
@@ -0,0 +1,1 @@
+define({  "name": "example-versioning",  "version": "0.2.0",  "description": "apiDoc versioning example",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2018-07-16T16:37:24.523Z",    "url": "http://apidocjs.com",    "version": "0.17.6"  }});
