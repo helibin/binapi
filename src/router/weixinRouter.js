b6d114534ba6817d28010340d789b7eaa392ce3d
何丽彬 <helibin@139.com>
2018-08-27 16:47:18 +0800

    微信服务回调IP

--- src/router/weixinRouter.js
+++ src/router/weixinRouter.js
@@ -2,7 +2,7 @@
  * @Author: helibin@139.com
  * @Date: 2018-08-23 15:11:25
  * @Last Modified by: lybeen
- * @Last Modified time: 2018-08-27 15:07:33
+ * @Last Modified time: 2018-08-27 16:46:08
  */
 /** 内建模块 */
 
@@ -10,12 +10,12 @@
 
 /** 基础模块 */
 import { router } from './base';
+import { CONFIG } from '../helper';
 
 /** 项目模块 */
 import { weixinCtrl } from '../controller';
 import { ipMid } from '../middleware';
 
 router.get('/wx/check-sign',
-  ipMid.allowAccess(),
-  ipMid.denyAccess(),
+  ipMid.allowAccess(CONFIG.wxServer.ipList),
   weixinCtrl.run('checkSign'));
