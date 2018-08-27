ed8911fbdf420718d0a3fad08c91550ca5d0e38c
何丽彬 <helibin@139.com>
2018-08-27 14:50:17 +0800

    weixinServer -> wxServer

--- src/controller/weixinCtrl.js
+++ src/controller/weixinCtrl.js
@@ -2,7 +2,7 @@
  * @Author: helibin@139.com
  * @Date: 2018-08-23 15:13:57
  * @Last Modified by: lybeen
- * @Last Modified time: 2018-08-27 14:47:13
+ * @Last Modified time: 2018-08-27 14:49:55
  */
 /** 内建模块 */
 
@@ -20,7 +20,7 @@ export default new class extends Base {
     const query = ctx.request.query;
     this.ret.data = query;
 
-    const signStr = [CONFIG.weixinServer.secret, query.timestamp, query.nonce].sort().join('');
+    const signStr = [CONFIG.wxServer.secret, query.timestamp, query.nonce].sort().join('');
     const sign = this.t.getSha1(signStr);
 
     if (sign !== query.signature) throw new this._e('EWeixinAPI', 'checkSignFailed');
